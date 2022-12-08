d1943bf1276e76f5aa78fd75e554849fdd0b7885
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-12-08 12:25:26 -0300

    add: handling post requests

--- app.js
+++ app.js
@@ -3,20 +3,11 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.status(200).json(
-//         {
-//             message: 'Hello from the server side!', 
-//             app: 'Natours'
-//         }
-//     );
-// })
+app.use(express.json()); // puts the body on the request
 
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint..')
-// })
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -25,6 +16,23 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
