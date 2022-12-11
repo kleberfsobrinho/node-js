4c13b43e655403ca01e201a46329b02d0664c987
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-12-11 00:14:12 -0300

    add: multiple routers

--- app.js
+++ app.js
@@ -119,24 +119,30 @@ const deleteUser = (req, res) => {
 }
 
 // 3) ROUTE
+const tourRouter = express.Router()
+const userRouter = express.Router()
 
-app
-    .route('/api/v1/tours')
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+
+tourRouter
+    .route('/')
     .get(getAllTours)
     .post(createTour)
-app
-    .route('/api/v1/tours/:id')
+
+tourRouter
+    .route('/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour)
 
-app
-    .route('/api/v1/users')
+userRouter
+    .route('/')
     .get(getAllUsers)
     .post(createUser)
 
-app
-    .route('/api/v1/users/:id')
+userRouter
+    .route('/:id')
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser)
