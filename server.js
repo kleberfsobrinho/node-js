3554937f00f46eb987e3761dc4c05ce3d3ddbdf6
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-12-14 19:23:48 -0300

    connecting database

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+
 dotenv.config({ path: './config.env' })
+
+DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connection successful!'))
 
 const app = require('./app')
 
