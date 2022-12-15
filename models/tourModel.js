2ab715b090b216deec6a58f2e1333780c84577b2
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-12-14 22:04:40 -0300

    update: tours model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,13 +6,55 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
+    },
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover']
+    },
+    images: {
+        type: [String]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: {
+        type: [Date]
     }
 });
 
