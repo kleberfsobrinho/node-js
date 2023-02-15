d5f3ef2bb43395962f93fd17a40fb783c5165457
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2023-02-15 20:19:29 -0300

    add: custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -8,7 +9,8 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         maxlength: [50, 'A tour name must be less than 50 characters'],
-        minlength: [3, 'A tour name must be more than 3 characters']
+        minlength: [3, 'A tour name must be more than 3 characters'],
+        // validate: [validator.isAlpha, 'A tour name can only contain letters']
     },
     slug: String,
     duration: {
@@ -42,7 +44,14 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a price']
     },
     priceDiscount: {
-        type: Number
+        type: Number,
+        validate: {
+            validator: function (value) {
+                // this - only works on create files 
+                return value < this.price
+            },
+            message: 'Discount price ({VALUE}) must be less than the price'
+        }
     },
     summary: {
         type: String,
