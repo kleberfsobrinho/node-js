636e38916723504635d84a48e870da4e2c66c5c3
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2023-02-17 15:42:25 -0300

    add: handling unhandled routes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
 }
+
 app.use(express.json()); // puts the body on the request
 app.use(express.static('./public')) // serve static files
 
@@ -18,5 +19,12 @@ app.use(express.static('./public')) // serve static files
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 404,
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+})
 
 module.exports = app;
